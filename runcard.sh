2f6f7516c6adc06e987ba90c7668218cc15b2e9d
ElStabilini <el.stabilini@gmail.com>
2024-08-20 15:08:35 +0200

    first test for bash script for runcard

---
+++ runcard.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# SBATCH directives (optional)
+#SBATCH --job-name=runcard_test0  # Job name
+#SBATCH --time=01:00:00            # Time limit
+#SBATCH --partition=iqm5q            # Partition name
+
+# Run the qq acquire command
+qq acquire /home/users/elisa.stabilini/qrctest/tutorial0.yml
+
